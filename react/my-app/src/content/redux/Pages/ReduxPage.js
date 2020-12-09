0fb401912b113492f01c07245d1cd661fbdffbfb
JasonXiang2014 <931477935@qq.com>
2020-12-10 00:00:48 +0800

    Use Redux CombineReducers

--- react/my-app/src/content/redux/Pages/ReduxPage.js
+++ react/my-app/src/content/redux/Pages/ReduxPage.js
@@ -44,15 +44,23 @@ export default class ReduxPage extends Component {
     );
   };
 
+  todo = () => {
+    store.dispatch({
+      type: 'ADD_TODO',
+      text: 'Use Redux'
+    })
+    console.log(store.getState())
+  }
   render() {
     return (
       <div>
         <h3>this is redux page</h3>
-        <p>{store.getState()}</p>
+        <p>{store.getState().counter}</p>
         <button onClick={this.add}>add</button>
         <button onClick={this.minus}>minus</button>
         <button onClick={this.asyAdd}>asyAdd</button>
         <button onClick={this.promiseMinus}>promise minus</button>
+        <button onClick={this.todo}>todo</button>
       </div>
     );
   }
--- react/my-app/src/content/redux/store/index.js
+++ react/my-app/src/content/redux/store/index.js
@@ -1,51 +1,42 @@
-// import { applyMiddleware, createStore } from 'redux'
-// import logger from 'redux-logger'
-// import thunk from 'redux-thunk'
-// import promise from 'redux-promise'
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise'
 
-import { applyMiddleware, createStore } from '../kredux'
+// import { applyMiddleware, createStore } from '../kredux'
 
-import isPromise from 'is-promise'
+// import isPromise from 'is-promise'
 
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "ADD":
-      return state + 1;
-    case "MINUS":
-      return state - 1;
-    default:
-      return state
-  }
-}
+import reducer from  '../reducers'
 
-const thunk = ({ dispatch, getState }) => {
-  return next => action => {
-    if (typeof action === 'function') {
-      return action(dispatch, getState)
-    }
-    return next(action)
-  }
-}
+// const thunk = ({ dispatch, getState }) => {
+//   return next => action => {
+//     if (typeof action === 'function') {
+//       return action(dispatch, getState)
+//     }
+//     return next(action)
+//   }
+// }
 
-const promise = ({ dispatch, getState }) => {
-  return next => action => {
-    return isPromise(action) ? action.then(dispatch) : next(action)
-  }
-}
+// const promise = ({ dispatch, getState }) => {
+//   return next => action => {
+//     return isPromise(action) ? action.then(dispatch) : next(action)
+//   }
+// }
 
-const logger = ({ dispatch, getState }) => {
-  return next => action => {
-    console.log('****************************************')
-    console.log(action.type + '执行了')
+// const logger = ({ dispatch, getState }) => {
+//   return next => action => {
+//     console.log('****************************************')
+//     console.log(action.type + '执行了')
 
-    console.log('pre state', getState())
+//     console.log('pre state', getState())
 
-    next(action)
-    console.log('next state', getState())
+//     next(action)
+//     console.log('next state', getState())
 
-    console.log('****************************************')
-  }
-}
+//     console.log('****************************************')
+//   }
+// }
 
 const store = createStore(reducer, applyMiddleware(thunk, promise, logger))
 
