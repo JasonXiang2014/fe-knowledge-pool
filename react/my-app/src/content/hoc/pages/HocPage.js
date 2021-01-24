3c9bdec0cda62ae561baf2be20fff7a7ec61f71a
JasonXiang2014 <931477935@qq.com>
2021-01-24 21:05:35 +0800

    熟悉一下装饰器的使用

--- react/my-app/src/content/hoc/pages/HocPage.js
+++ react/my-app/src/content/hoc/pages/HocPage.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import HomePage from "../../private-route/pages/HomePage";
 
 //HOC Higher-Order Componnets
 //定义： 高阶组件是参数为组件，返回值为新组件的函数
 
 //react decorator 配置
 //https://stackoverflow.com/questions/52262084/syntax-error-support-for-the-experimental-syntax-decorators-legacy-isnt-cur
-const foo = (name) => Cmp => props => {
+const foo = ({ name }) => Cmp => props => {
   return (
     <div className="border">
       <Cmp {...props} name={name}></Cmp>
@@ -21,13 +22,49 @@ const foo2 = Cmp => props => {
   )
 }
 
-//装饰器只能⽤在class上
-//执⾏顺序从下往上
+function testable2(isTestable) {
+  return function (target) {
+    target.isTestable = isTestable;
+  }
+}
+
+function testable(traget) {
+  console.log('xjDebugger:traget', traget)
+  traget.isTestable = true
+}
+
+function dec(id) {
+  console.log('evaluated', id);
+  return (target, property, descriptor) => console.log('executed', id);
+}
+
+function log(target, name, descriptor) {
+  console.log('xjDebugger:readonly', target, name, descriptor)
+  var oldValue = descriptor.value
+  descriptor.value = function () {
+    console.log(`Calling ${name} with`, arguments);
+    return oldValue.apply(this, arguments);
+  }
+  return descriptor
+}
+
+//先从外到内进入，然后由内向外执行。
+
 @foo2
-@foo('xj')
+@foo({ name: 'xj' })
+@testable2(false)
+@testable
 class HocPage extends Component {
   state = {};
+
+  @dec(1)
+  @log
+  add(a, b) {
+    return a + b
+  }
+
   render() {
+    console.log('xjDebugger', HocPage.isTestable, this.add(1, 2))
     return (
       <div>
         <h3>HOC page</h3>
@@ -47,6 +84,6 @@ function Child(props) {
   </div>
 }
 
-const Foo = foo('xj')(Child)
+const Foo = foo({ name: 'xj' })(Child)
 //链式调用
-const Foo2 = foo2(foo2(foo('learn to use decorator')(Child)))
+const Foo2 = foo2(foo2(foo({ name: 'learn to use decorator' })(Child)))
