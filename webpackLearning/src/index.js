ff876d3bb3b680239c7badb86a72da298ae14904
JasonXiang2014 <xiang_jerry@sina.com>
2021-04-19 17:25:51 +0800

    loader

--- webpackLearning/src/index.js
+++ webpackLearning/src/index.js
@@ -1,2 +1,5 @@
-import { str } from './a.js'
+import css from './index.css'
+import pic from './logo.jpg'
+import { str } from './a.js' // * webpack对前端来说只支持js模块和json模块 .js .json
 console.log(`${str} webpack`)
+console.log(pic)
--- webpackLearning/webpack.xj.js
+++ webpackLearning/webpack.xj.js
@@ -17,6 +17,20 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     //资源名称 name就是entry里面的key值 占位符的概念 【name】【hash:number】【chunkhash】【contenthash】
     filename: "[name]-[hash:6].js",
+  },
+  module: { //让webpack支持更多的模块 (webpack对前端来说只支持js模块和json模块)
+    //css-loader 只是支持webpack解析css文件，但是还无法使用
+    rules: [{
+      test: /\.css$/,
+      //多个loader 是有执行顺序的，自后往前
+      //style-loader Inject CSS into the DOM.
+      use: ["style-loader", "css-loader",]
+    }, {
+      test: /\.(png|jpg|gif)$/i,
+      //多个loader 是有执行顺序的，自后往前
+      //style-loader Inject CSS into the DOM.
+      use: ["url-loader"]
+    }],
   }
 }
 
