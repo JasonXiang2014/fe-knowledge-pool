bd44adc653b45704db6fd80660becedbdf88283e
JasonXiang2014 <xiang_jerry@sina.com>
2021-04-19 16:21:04 +0800

    entry object写法

--- webpackLearning/webpack.xj.js
+++ webpackLearning/webpack.xj.js
@@ -1,12 +1,16 @@
 const path = require("path")
 module.exports = {
   //入口：
-  entry: "./src/index.js",
+  //string array object
+  // entry: "./src/index.js",
+  entry: {
+    main: "./src/index.js", // object 写法
+  },
   mode: "development",
   //出口：
   output: {
     //输出资源的存放位置，必须是绝对路径
-    path: path.resolve(__dirname, "./xj"),
+    path: path.resolve(__dirname, "./dist"),
     //资源名称
     filename: "xj.js",
   }
