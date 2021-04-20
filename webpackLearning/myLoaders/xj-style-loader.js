241d7fb420e5a32c842004a544aa457712fd71c3
JasonXiang2014 <xiang_jerry@sina.com>
2021-04-20 16:41:18 +0800

    实现自定义的style-loader、css-loader、less-loader

---
+++ webpackLearning/myLoaders/xj-style-loader.js
@@ -0,0 +1,7 @@
+
+module.exports = function (source) {
+  return `const e = document.createElement('style')
+  e.innerHTML = ${source}
+  document.head.appendChild(e)
+  `;
+}
---
+++ webpackLearning/webpack.xj3.js
@@ -0,0 +1,78 @@
+const path = require("path")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { options } = require("less");
+
+module.exports = {
+  //入口：
+  entry: {
+    index: "./src/index.js",
+    index2: "./src/index2.js",
+  },
+  mode: "development",
+  //出口：
+  output: {
+    //输出资源的存放位置，必须是绝对路径
+    path: path.resolve(__dirname, "./dist"),
+    //资源名称
+    filename: "[name]-[chunkhash:6].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', "css-loader"]
+      },
+      {
+        test: /\.less$/,
+        use: ["xj-style-loader", "xj-css-loader", "xj-less-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        //多个loader 是有执行顺序的，自后往前
+        //style-loader Inject CSS into the DOM.
+        use: ["url-loader"]
+      },
+      {
+        test: /\.js$/,
+        // use: path.resolve(__dirname, "myLoaders/replace-loader.js"),
+        // use: {
+        //   loader: path.resolve(__dirname, "myLoaders/replace-loader.js"),
+        //   options: {
+        //     name: 'xj'
+        //   }
+        // },
+        use: [
+          "replace-loader.js",
+          {
+            loader: "replace-loader-async.js",
+            options: {
+              name: 'xj'
+            }
+          },
+        ],
+        exclude: /dist/
+      }
+    ]
+  },
+  resolveLoader: {
+    modules: ["node_modules", "./myLoaders"]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: ["index"],
+    }),
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index2.html',
+      chunks: ["index2"],
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/index-[chunkhash:6].css',
+    })
+  ]
+}
