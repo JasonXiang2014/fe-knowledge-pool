8d6c5737ce5e797042cefe851bde25689416547e
JasonXiang2014 <xiang_jerry@sina.com>
2021-04-20 17:24:46 +0800

    browserlist使用方式

--- webpackLearning/postcss.config.js
+++ webpackLearning/postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
       //兼容can I use浏览器的最近两个版本
       //兼容市场占有率大于1%的浏览器
       //覆盖package.json配置的browserList
-      overrideBrowserslist: ["last 2 versions", ">1%"]
+      // overrideBrowserslist: ["last 2 versions", ">1%"]
     })
   ]
 }
