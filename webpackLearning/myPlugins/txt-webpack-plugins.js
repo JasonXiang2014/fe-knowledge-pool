1ce46260c8a0e9237cd1aae169543c6c7efea802
JasonXiang2014 <xiang_jerry@sina.com>
2021-04-22 14:55:28 +0800

    自定义txtWebpackPlugins

---
+++ webpackLearning/myPlugins/txt-webpack-plugins.js
@@ -0,0 +1,28 @@
+const { compilation } = require("webpack")
+
+class txtWebpackPlugin {
+  constructor(options) {
+    console.log(options)
+  }
+  //如何钩入hooks
+  apply(compiler) {
+    //异步钩子
+    compiler.hooks.emit.tapAsync("txtWebpackPlugin", (compilation, cb) => {
+      compilation.assets["test.txt"] = {
+        source: function () {
+          return "hello myplugins"
+        },
+        size: function () {
+          return 1024
+        }
+      }
+      cb()
+    })
+
+    compiler.hooks.compile.tap("txtWebpackPlugin", (compilation) => {
+      console.log('hello sync hooks')
+    })
+  }
+}
+
+module.exports = txtWebpackPlugin
